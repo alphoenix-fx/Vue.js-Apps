2517f266d8821d2e6c1fa169c6e57e654ca46d4e
Alfred Maboa <34593342+alphoenixbiz@users.noreply.github.com>
2020-03-05 15:03:53 +0200

    Vue with Users API

---
+++ simple_vue_app/src/components/Users.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div class="container">
+    <h3>Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+
+<script>
+import axios from "axios";
+
+export default{
+    name: 'Users',
+    data(){
+        return {
+            users: null,
+        };
+    },
+    created: function(){
+        axios
+    //    .get("https://cors-anywhere.herokuapp.com/https://38203rhkf7.execute-api.eu-west-1.amazonaws.com/dev/${inputValue}")
+        .get('https://jsonplaceholder.typicode.com/users')
+          .then(res=>{
+           this.users = res.data;
+       })
+    }
+}
+</script>
+
+
+<style>
+h3{
+    margin-bottom: 5%;
+}
+
+</style>  
